d5d2c184c8d4e44eaaf2dace9ac24049c893b1be
Michael Jola-Moses <jolamoses@gmail.com>
2024-03-04 11:30:23 +0100

    Installed lodash cloneNeep

--- src/hooks/usePostComments.ts
+++ src/hooks/usePostComments.ts
@@ -4,7 +4,7 @@ import {
   useMutation,
   useQueryClient,
 } from '@tanstack/react-query';
-import cloneDeep from 'lodash/cloneDeep';
+import cloneDeep from 'lodash.clonedeep';
 
 import { CreatePostCommentOptions, EditPostCommentOptions } from '@/types/post';
 
